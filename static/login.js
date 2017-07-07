f5a4feae7778479c96a6cb918143c24f472ca862
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-07-07 18:20:51 +0600

    Рефакторинг формы и скрипта

---
+++ static/login.js
@@ -0,0 +1,45 @@
+(function () {
+
+  const loginForm = document.getElementById('loginForm');
+  const errorMessage = document.getElementById('errorMessage');
+
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const { login, password } = loginForm;
+    if (!login.value || !password.value) return errorShake();
+
+    const body = JSON.stringify({
+      login: login.value,
+      password: password.value
+    });
+
+    fetch('https://fake-admin-api.glitch.me/login', {
+      method: 'post',
+      headers: { "Content-type":"application/json" },
+      body
+    })
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
+      return jsonResponse;
+    })
+    .then( sessionData => {
+      const expires = new Date(new Date().getTime() + 60 * 1000);
+      Cookies.set('user_session', sessionData.session ,{ expires });
+      alert('redirect!');
+    })
+    .catch(error => {
+      errorMessage.style.display = 'block';
+      errorMessage.innerHTML = error.message;
+      errorShake();
+    });
+  });
+
+  function errorShake() {
+    const loginContainer = document.getElementById('loginContainer');
+    loginContainer.classList.add('shake');
+    setTimeout(() => loginContainer.classList.remove('shake'), 1000);
+  }
+
+})();
+
