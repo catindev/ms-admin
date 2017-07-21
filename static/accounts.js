f25c117a4ce1989af057f98de990f2147116b8c1
exoriri <lavrikrom2@gmail.com>
2017-07-21 16:16:21 +0600

    исправление пробелов,скобок и тд.

--- static/accounts.js
+++ static/accounts.js
@@ -1,53 +1,55 @@
 (function functionName() {
-const adminName = document.getElementById('adminName');
-const errorBlock = document.getElementById('errorBlock');
+  const adminName = document.getElementById("adminName");
+  const errorBlock = document.getElementById("errorBlock");
 
-//пользователь
-const userSession = Cookies.get('user_session');
-if (!userSession){
-  window.location.replace('index.html');
-}else {
-  fetch('https://fake-admin-api.glitch.me/users/'+userSession)
-  .then(response => response.json())
-  .then(jsonResponse => {
-    if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
-    return jsonResponse;
-  })
-  .then( userData => {
-    adminName.innerHTML += ' ' + userData.username;
-  })
-  .catch(error => {
-    errorBlock.style.display = 'block';
-    errorBlock.innerHTML = error.message;
-  });
+  //пользователь
+  const userSession = Cookies.get("user_session");
+  if (!userSession) {
+    window.location.replace("index.html");
+  } else {
+    fetch("https://fake-admin-api.glitch.me/users/" + userSession)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
+        return jsonResponse;
+      })
+      .then(userData => {
+        adminName.innerHTML += " " + userData.username;
+      })
+      .catch(error => {
+        errorBlock.style.display = "block";
+        errorBlock.innerHTML = error.message;
+      });
 
-  //аккаунты
-  fetch('https://fake-admin-api.glitch.me/accounts?user_session='+userSession)
-  .then(response => response.json())
-  .then(jsonResponse => {
-    if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
-    return jsonResponse;
-  })
-  .then( accounts => {
-    let companies = accounts.items;
-    const accountsList = document.getElementById('accountsList');
-    for (let index=0; index < companies.length; index++){
-      let li = document.createElement('li');
-      let a = document.createElement('a');
-      for(let companyData in companies[index]){
-        if (companyData === 'id'){
-          a.href = '/accounts/'+companies[index]['id']}
-        else if (companyData === 'name'){
-          a.innerHTML = companies[index]['name'] }
-      }
-      accountsList.appendChild(li);
-      li.appendChild(a);
-    }
-  })
-  .catch(error => {
-    errorBlock.style.display = 'block';
-    errorBlock.innerHTML = error.message;
-  });
-};
-
-}())
+    //аккаунты
+    fetch(
+      "https://fake-admin-api.glitch.me/accounts?user_session=" + userSession
+    )
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
+        return jsonResponse;
+      })
+      .then(accounts => {
+        let companies = accounts.items;
+        const accountsList = document.getElementById("accountsList");
+        for (let index = 0; index < companies.length; index++) {
+          let li = document.createElement("li");
+          let a = document.createElement("a");
+          for (let companyData in companies[index]) {
+            if (companyData === "id") {
+              a.href = "/accounts/" + companies[index]["id"];
+            } else if (companyData === "name") {
+              a.innerHTML = companies[index]["name"];
+            }
+          }
+          accountsList.appendChild(li);
+          li.appendChild(a);
+        }
+      })
+      .catch(error => {
+        errorBlock.style.display = "block";
+        errorBlock.innerHTML = error.message;
+      });
+  }
+})();
--- static/login.js
+++ static/login.js
@@ -1,9 +1,8 @@
-(function () {
+(function() {
+  const loginForm = document.getElementById("loginForm");
+  const errorMessage = document.getElementById("errorMessage");
 
-  const loginForm = document.getElementById('loginForm');
-  const errorMessage = document.getElementById('errorMessage');
-
-  loginForm.addEventListener('submit', function(event) {
+  loginForm.addEventListener("submit", function(event) {
     event.preventDefault();
     const { login, password } = loginForm;
     if (!login.value || !password.value) return errorShake();
@@ -12,32 +11,31 @@
       login: login.value,
       password: password.value
     });
-    fetch(' https://fake-admin-api.glitch.me/login', {
-      method: 'post',
-      headers: { "Content-type":"application/json" },
+    fetch(" https://fake-admin-api.glitch.me/login", {
+      method: "post",
+      headers: { "Content-type": "application/json" },
       body
     })
-    .then(response => response.json())
-    .then(jsonResponse => {
-      if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
-      return jsonResponse;
-    })
-    .then( sessionData => {
-      const expires = new Date(new Date().getTime() + 60 * 1000);
-      Cookies.set('user_session', sessionData.session ,{ expires });
-      window.location.replace('accounts.html');
-    })
-    .catch(error => {
-      errorMessage.style.display = 'block';
-      errorMessage.innerHTML = error.message;
-      errorShake();
-    });
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
+        return jsonResponse;
+      })
+      .then(sessionData => {
+        const expires = new Date(new Date().getTime() + 60 * 1000);
+        Cookies.set("user_session", sessionData.session, { expires });
+        window.location.replace("accounts.html");
+      })
+      .catch(error => {
+        errorMessage.style.display = "block";
+        errorMessage.innerHTML = error.message;
+        errorShake();
+      });
   });
 
   function errorShake() {
-    const loginContainer = document.getElementById('loginContainer');
-    loginContainer.classList.add('shake');
-    setTimeout(() => loginContainer.classList.remove('shake'), 1000);
+    const loginContainer = document.getElementById("loginContainer");
+    loginContainer.classList.add("shake");
+    setTimeout(() => loginContainer.classList.remove("shake"), 1000);
   }
-
 })();
