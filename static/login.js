bada040c7727ecd70a5c19594b64458caf802b67
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-21 17:04:30 +0600

    Fix login API integration

--- static/login.js
+++ static/login.js
@@ -10,7 +10,7 @@
       login: login.value,
       password: password.value
     });
-    fetch(Config.API_HOST + "/login", {
+    fetch(Config.API_HOST + "/sessions", {
       method: "post",
       headers: { "Content-type": "application/json" },
       body
@@ -20,9 +20,9 @@
         if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
         return jsonResponse;
       })
-      .then(sessionData => {
+      .then(({ token }) => {
         const expires = new Date(new Date().getTime() + 31536000000);
-        Cookies.set("user_session", sessionData.session, { expires });
+        Cookies.set("user_session", token, { expires });
         window.location.replace("/accounts");
       })
       .catch(error => {
