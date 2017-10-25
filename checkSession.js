1588801758262c8f03c1f24f162067781f135199
exoriri <lavrikrom2@gmail.com>
2017-10-25 09:31:59 +0600

    rewrited checksession function

--- checkSession.js
+++ checkSession.js
@@ -1,9 +1,6 @@
 module.exports = function (request,response,next) {
   const { user_session } = request.cookies;
-  next()
-  if (!user_session && request.url !== '/') {
-    return response.redirect('/');
-  }else if (user_session && request.url === '/') {
-    response.redirect('/accounts');
-  }
+  if (!user_session && request.url !== '/')return response.redirect('/');
+  if (user_session && request.url === '/') return response.redirect('/accounts');
+  next();
 }
