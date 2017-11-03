78eae384a9336bcb8d07df3fe218e5eeeda14be5
exoriri <lavrikrom2@gmail.com>
2017-11-03 16:09:07 +0600

    fix session bug

--- checkSession.js
+++ checkSession.js
@@ -1,6 +1,8 @@
 module.exports = function (request,response,next) {
   const { user_session } = request.cookies;
-  if (!user_session && request.url !== '/')return response.redirect('/');
+
+  if (!user_session && request.url !== '/') return response.redirect('/');
   if (user_session && request.url === '/') return response.redirect('/accounts');
+  
   next();
 }
