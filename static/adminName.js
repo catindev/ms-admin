6ab70388216b47959f7185e3bbae95cc16547f33
exoriri <lavrikrom2@gmail.com>
2017-11-17 16:32:20 +0600

    fix #104

--- static/adminName.js
+++ static/adminName.js
@@ -10,8 +10,11 @@ const exitBtn = document.getElementById('exitBtn');
             return jsonResponse;
         })
         .then(userData => {
-            if (location.pathname === '/journal' && userData.access !== 'admin') {
+            if (location.pathname === '/journal' && userData.access === 'admin') {
               throw Error('Доступ закрыт');
+            }
+            if (url.includes('/trunks') && userData.access === 'admin'){
+              statTabs.style.display = 'flex';
             }
             if (userData.access === 'admin') {
               journal.style.display = 'block';
